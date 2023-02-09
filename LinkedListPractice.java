d2cd018105f50b2219445f89195d234434053056
Justin <justinrkinsman@gmail.com>
2023-02-09 09:41:40 -0400

    Add removeFirst() to linkedlist

--- LinkedListPractice.java
+++ LinkedListPractice.java
@@ -13,49 +13,54 @@ public class LinkedListPractice<E> /* implements List<E> */ {
 
     private Node<E> head;
     private int currentSize;
-    // private Node<E> tail;
+    private Node<E> tail;
 
     public LinkedListPractice() {
         head = null;
         currentSize = 0;
     }
 
+    // add tail to list if list is empty or if list only has one element
     public void addFirst(E obj) {
         Node<E> node = new Node<E>(obj);
+        if (head == null) {
+            head = tail = node;
+            currentSize++;
+            return;
+        }
         node.next = head;
         head = node;
         currentSize++;
     }
 
-    public void addLast(E obj) { // O(n)
+    public void addLast(E obj) {
         Node<E> node = new Node<E>(obj);
         if (head == null) {
-            head = node;
+            head = tail = node;
             currentSize++;
+            return;
         }
-        Node<E> tmp = head;
-        while (tmp.next != null) {
-            tmp = tmp.next;
-        }
-        tmp.next = node;
+        tail.next = node;
+        tail = node;
         currentSize++;
+        return;
     }
 
-    /*
-     * ALT addLast O(1)
-     * public void addLast(E obj) {
-     * Node<E> node = new Node<E>(obj);
-     * if (head == null) {
-     * head = tail = node;
-     * currentSize++;
-     * return;
-     * }
-     * tail.next = node;
-     * tail = node;
-     * currentSize++;
-     * return;
-     * }
-     * tail pointer needs to be added to addFirst() as well
-     */
+    public E removeFirst() {
+        if (head == null) {
+            return null;
+        }
+        E tmp = head.data;
+        if (head.next == null) {
+            // if (currentSize == 1)
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+        currentSize--;
+        return tmp;
+    }
 
 }
+
+// add tail pointer to addFirst()
