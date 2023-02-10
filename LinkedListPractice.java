fcf686ec3139be49fc3f8da703d761124f8795c1
Justin <justinrkinsman@gmail.com>
2023-02-10 09:59:11 -0400

    Add additional methods to linkedlistpractice

--- LinkedListPractice.java
+++ LinkedListPractice.java
@@ -1,6 +1,9 @@
-//import java.util.List;
 
-public class LinkedListPractice<E> /* implements List<E> */ {
+//import java.util.List;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedListPractice<E> {
     class Node<E> {
         E data;
         Node<E> next;
@@ -8,6 +11,27 @@ public class LinkedListPractice<E> /* implements List<E> */ {
         public Node(E obj) {
             data = obj;
             next = null;
+        }
+    }
+
+    class IteratorHelper implements Iterator<E> {
+        Node<E> index;
+
+        public IteratorHelper() {
+            index = head;
+        }
+
+        public boolean hasNext() {
+            return (index != null);
+        }
+
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            E value = index.data;
+            index = index.next;
+            return value;
         }
     }
 
@@ -77,6 +101,51 @@ public class LinkedListPractice<E> /* implements List<E> */ {
         tail = previous;
         currentSize--;
         return current.data;
+    }
+
+    public E remove(E obj) {
+        Node<E> current = head, previous = null;
+        while (current != null) {
+            if (((Comparable<E>) current.data).compareTo(obj) == 0) {
+                if (current == head) {
+                    return removeFirst();
+                }
+                if (current == tail) {
+                    return removeLast();
+                }
+                currentSize--;
+                previous.next = current.next;
+                return current.data;
+            }
+            previous = current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    public boolean contains(E obj) {
+        Node<E> current = head;
+        while (current != null) {
+            if (((Comparable<E>) current.data).compareTo(obj) == 0) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public E peekFirst() {
+        if (head == null) {
+            return null;
+        }
+        return head.data;
+    }
+
+    public E peekLast() {
+        if (tail == null) {
+            return null;
+        }
+        return tail.data;
     }
 
 }
---
+++ Tester.java
@@ -0,0 +1,22 @@
+/*
+ * import java.util.List;
+ * 
+ * public class Tester {
+ * public static void main(String[] args) {
+ * List<Integer> list = new LinkedListPractice<Integer>();
+ * int n = 10;
+ * for (int i = 0; i < n; i++) {
+ * list.addFirst(i);
+ * }
+ * for (int i = n - 1; i >= 0; i--) {
+ * int x = list.removeFirst();
+ * if (x != i) {
+ * //...
+ * }
+ * }
+ * for (int i = 0; i < n; i ++){
+ * int x = list.removeLast();
+ * }
+ * }
+ * }
+ */
