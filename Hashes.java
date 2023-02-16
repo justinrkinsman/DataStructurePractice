0a60d11b32d12919a37a32668a7b8e3eb7498bf6
Justin <justinrkinsman@gmail.com>
2023-02-16 09:41:24 -0400

    Add hashcode to hashes function

--- Hashes.java
+++ Hashes.java
@@ -1,3 +1,10 @@
 public class Hashes {
-
+    public int hashCode(String s) {
+        int g = 31;
+        int hash = 0;
+        for (int i = 0; i < s.length(); i++) {
+            hash = g * hash + s.charAt(i);
+        }
+        return hash;
+    }
 }
