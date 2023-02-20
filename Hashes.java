1096afb40d51f6aa4b0ac71115a2b9cf85ef8b3e
Justin <justinrkinsman@gmail.com>
2023-02-20 08:36:37 -0400

    Declare global variables in hashes.java

--- Hashes.java
+++ Hashes.java
@@ -1,10 +1,19 @@
-public class Hashes {
-    public int hashCode(String s) {
-        int g = 31;
-        int hash = 0;
-        for (int i = 0; i < s.length(); i++) {
-            hash = g * hash + s.charAt(i);
+public class Hashes<K, V> /* implements HashI<K, V> */ {
+    class HashElement<K, V> implements Comparable<HashElement<K, V>> {
+        K key;
+        V value;
+
+        public HashElement(K key, V value) {
+            this.key = key;
+            this.value = value;
         }
-        return hash;
+
+        public int compareTo(HashElement<K, V> o) {
+            return (((Comparable<K>) this.key).compareTo(o.key));
+        }
     }
+
+    int numElements /* current size */, tableSize /* size of array */;
+    double maxLoadFactor /* the point at which we resize */;
+    LinkedListPractice<HashElement<K, V>>[] hashArray; /* The hash array will be an array of linked lists */
 }
