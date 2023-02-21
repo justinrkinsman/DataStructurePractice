587dae3a5a6e49151978f6054babde7754232a3a
Justin <justinrkinsman@gmail.com>
2023-02-21 10:02:20 -0400

    Create heap class

--- Heap.java
+++ Heap.java
@@ -1,3 +1,27 @@
-public class Heap {
+public class Heap<E> {
+    int lastPosition;
+    int size;
+    E[] array = (E[]) new Object[size];
 
+    public void add(E obj) {
+        array[++lastPosition] = obj;
+        trickleUp(lastPosition);
+    }
+
+    public void swap(int from, int to) {
+        E tmp = array[from];
+        array[from] = array[to];
+        array[to] = tmp;
+    }
+
+    public void trickleUp(int position) {
+        if (position == 0) {
+            return;
+        }
+        int parent = (int) Math.floor((position - 1) / 2);
+        if (((comparable<E>) array[position]).compareTo(array.parent) > 0) {
+            swap(position, parent);
+            trickleUp(parent);
+        }
+    }
 }
