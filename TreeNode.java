9a98491243bff3978c95a4e0e39a6939d88b6dac
Justin <justinrkinsman@gmail.com>
2023-02-22 11:57:19 -0400

    Create add and contains methods for tree

--- TreeNode.java
+++ TreeNode.java
@@ -6,4 +6,46 @@ public class TreeNode<E> {
         this.data = obj;
         left = right = null;
     }
+
+    public void add(E obj, TreeNode<E> node) {
+        if (((Comparable<E>) obj).compareTo(node.data) > 0) /* if object is bigger than data, go to right */ {
+            if (node.right == null) {
+                node.right = new TreeNode<E>(obj);
+                return;
+            }
+            return add(obj, node.right);
+        }
+        if (node.left == null) {
+            node.left = new TreeNode<E>(obj);
+            return;
+        }
+        return add(obj, node.left);
+    }
+
+    public void add(E obj) {
+        if (root == null) {
+            root = new TreeNode<E>(obj);
+        } else {
+            add(obj, root);
+        }
+        currentSize++;
+    }
+
+    public boolean contains(E obj) {
+        return contains(obj, root);
+    }
+
+    private boolean contains(E obj, TreeNode<E> node) {
+        if (node == null) {
+            return false;
+        }
+        if (((Comparable<E>) obj).compareTo(node.data) == 0) {
+            return true;
+        }
+        if (((Comparable<E>) obj).compareTo(node.data) > 0) {
+            return contains(obj, node.right);
+        }
+        return contains(obj, node.left);
+    }
+
 }
